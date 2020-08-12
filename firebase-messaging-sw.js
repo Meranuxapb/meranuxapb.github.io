9bd06920e9b5bd58dd9744d4bd9a5db07dd9a8f6
Anton Sidorenko <asidorenko@surfy.ru>
2020-08-12 17:14:43 +0500

    Add firebase debug page

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,42 @@
+﻿importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
+importScripts(
+    "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js",
+);
+// For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
+importScripts(
+    "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js",
+);
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+    apiKey: "AIzaSyCEM2HLkra5BH56n6q-z-gsjKkDjdDOEL8",
+    authDomain: "testproject-78068.firebaseapp.com",
+    databaseURL: "https://testproject-78068.firebaseio.com",
+    projectId: "testproject-78068",
+    storageBucket: "testproject-78068.appspot.com",
+    messagingSenderId: "60581419651",
+    appId: "1:60581419651:web:4fe8a3e4536dad2eba99b6"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log(
+        "[firebase-messaging-sw.js] Received background message ",
+        payload,
+    );
+    // Customize notification here
+    const notificationTitle = "Background Message Title";
+    const notificationOptions = {
+        body: "Background Message body.",
+        icon: "/itwonders-web-logo.png",
+    };
+
+    return self.registration.showNotification(
+        notificationTitle,
+        notificationOptions,
+    );
+});
